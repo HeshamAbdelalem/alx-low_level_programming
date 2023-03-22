6df10f5d3262439f6525905babd15be63ce6b213
e <hesham.abdelalem1@gmail.com>
2023-03-22 15:52:56 +0200

    0x02

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.c"
+/**
+ * _isalpha - shows 1 if the char a , if not 0
+ *
+ * @c: the char
+ *
+ * Return: 1 for a , 0 if not
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n')
+}
