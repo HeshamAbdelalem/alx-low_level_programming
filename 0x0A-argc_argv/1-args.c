4ef7164d7f3cad0699b94881170572842768584d
HeshamAbdelalem <hesham.abdelalem1@gmail.com>
2023-06-10 00:06:28 +0300

    adds 1-argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the arguments passed to the function
+ * @argc: argument count
+ * @argv: arguments vector array
+ *
+ * Return: 0 [nothing actually]
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%i\n", argc);
+return 0;
+}
