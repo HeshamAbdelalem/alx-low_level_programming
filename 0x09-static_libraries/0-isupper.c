efae2a1521e1ce7d0cbcc97f80b5011085b720d2
HeshamAbdelalem <hesham.abdelalem1@gmail.com>
2023-06-14 15:14:47 +0300

    0x09 static lib

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase char
+ * @c: the chara to be checked
+ * Return: 1 if c uppercase, 0 if not
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,17 +1,12 @@
 #include <unistd.h>
 
 /**
- ** _putchar - writes the character c to stdout
+ * _putchar - writes the character c
+ * @c: The character to print
  *
- ** @c: The character to print
- *
- **
- *
- ** Return: On success 1.
- *
- ** On error, -1 is returned, and errno is set appropriately
+ * Return: On success 1.
+ * On error, -1 is returned
  */
-
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
